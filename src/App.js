7d77e83d37fa9f924d2db48ae54ff781dbff738b
AipBorges <mpbds@uic.es>
2021-10-19 19:59:09 +0200

    added some styling to make it visible in homepage and corrected a few typos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Hangman from "./components/Hangman/Hangman";
+import "./App.css";
+import Button from "./components/Hangman/Button";
+import { useState } from "react";
+import Input from "./components/Hangman/Input";
+import WrongAnswersContainer from "./components/WrongAnswersContainer";
+import LetterContainer from "./components/Hangman/letterContainer/LetterContainer";
 
 function App() {
+  const [myWords, setMyWords] = useState([{ word: "perro", id: 1 }]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>The Hangman</h1>
+        <Hangman />
+        <LetterContainer letter="e"></LetterContainer>
+        <Button text="ok" actionOnClick={() => console.log("holi")} />
+        <Input maxNumber={0} />
+        <WrongAnswersContainer wrongLetter="a" />
       </header>
     </div>
   );
